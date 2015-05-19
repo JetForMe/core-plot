fdf760941e4e6228586681e05673471ded0d657b
Rick <rmann@latencyzero.com>
2015-05-18 23:33:05 -0700

    -removeBackgroundLimitBand: nil now removes all bands.
    There was no means to remove all limit bands. Now you can pass nil to -removeBackgroundLimitBand: to remove all bands.

--- framework/Source/CPTAxis.m
+++ framework/Source/CPTAxis.m
@@ -2007,6 +2007,8 @@ NSDecimal CPTNiceLength(NSDecimal length)
 
         CPTPlotArea *thePlotArea = self.plotArea;
         [thePlotArea setNeedsDisplay];
+    } else {
+        [self.mutableBackgroundLImitBands removeAllObjects];
     }
 }
 
